2992dfd510dbf2743a152efc7b14a66e8baf248f
Anna Leontyeva <99910839+annlntv@users.noreply.github.com>
2023-07-13 18:23:20 +0500

    Update PlayerUI.cs

--- PlayerUI.cs
+++ PlayerUI.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class PlayerUI : MonoBehaviour
 {
+	public GameObject canvas;
 	public KeysCount keysCount;
 	public TextMeshProUGUI keysCounterText;
 
@@ -13,6 +15,20 @@ public class PlayerUI : MonoBehaviour
 	{
 		// Обновляем текст с кол-вом ключей
 		keysCounterText.text = "Собрано ключей: " + keysCount.keys.ToString() + "/ 3";
+
+		if(keysCount.keys == 3)
+        {
+			canvas.SetActive(true);
+			StartCoroutine(RestartLevelDelayed());
+
+		}
+	}
+	IEnumerator RestartLevelDelayed()
+	{
+		yield return new WaitForSeconds(3f);
+
+		Time.timeScale = 1;
+		SceneManager.LoadScene(0);
 	}
 }
 
